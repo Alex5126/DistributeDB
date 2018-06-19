37ca7fdf47a108b2270f3c20c3c33a0c4121ed2f
Jonathan Bravo <jonhdeveloper@outlook.com>
2018-06-19 01:27:01 -0500

    UI

--- DistributedDB/Program.cs
+++ DistributedDB/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleTables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,10 +16,23 @@ namespace DistributedDB
             var top = Application.Top;
             var win = new Window(new Rect(0, 1, top.Frame.Width, top.Frame.Height - 1), "Distribuited DB App");
             top.Add(win);
-            var labelQuery = new Label(3, 2, "Query: ");
-            var query = new TextField(14, 2, 40, "");
-            var consult = new Button(60, 2, "Ok");
-            top.Add(labelQuery, query, consult);
+            var labelQuery = new Label(3, 3, "Query: ");
+            var query = new TextField(14, 3, 40, "");
+            var consult = new Button(60, 3, "Ok");
+            var textView = new TextView(new Rect(3, 7, 115, 70))
+            {
+                Text = "Esta es una prueba",
+                
+            };
+
+            var table = new ConsoleTable("one", "two", "three");
+            table.AddRow(1, 2, 3)
+                 .AddRow("this line should be longer", "yes it is", "oh");
+
+            var tableText = table.ToString();
+
+            textView.Text = tableText;
+            win.Add(labelQuery, query, consult, textView);
             Application.Run();
         }
 	}
