4057be4a3cca1fb49903bd62e47aea898ea9f689
Jonathan Bravo <jonhdeveloper@outlook.com>
2018-06-19 00:52:50 -0500

    UI

--- DistributedDB/Program.cs
+++ DistributedDB/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terminal.Gui;
 
 namespace DistributedDB
 {
@@ -10,7 +11,15 @@ namespace DistributedDB
 	{
 		static void Main(string[] args)
 		{
-
-		}
+            Application.Init();
+            var top = Application.Top;
+            var win = new Window(new Rect(0, 1, top.Frame.Width, top.Frame.Height - 1), "Distribuited DB App");
+            top.Add(win);
+            var labelQuery = new Label(3, 2, "Query: ");
+            var query = new TextField(14, 2, 40, "");
+            var consult = new Button(60, 2, "Ok");
+            top.Add(labelQuery, query, consult);
+            Application.Run();
+        }
 	}
 }
