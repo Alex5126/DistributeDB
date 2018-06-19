6710fc77def2dc51adbe4ce497e76dc1147469d4
Jonathan Bravo <jonhdeveloper@outlook.com>
2018-06-19 04:36:32 -0500

    Le movi a lo inmovible :v

--- DistributedDB/Helpers/StaticConstants.cs
+++ DistributedDB/Helpers/StaticConstants.cs
@@ -13,8 +13,8 @@ namespace DistributedDB.Helpers
         public static string BD2 = "Data Source=JONHDEVPC\\SQLEXPRESS;Initial Catalog = Agencia2;Integrated Security=true;";
         public static string BD3 = "Data Source=JONHDEVPC\\SQLEXPRESS;Initial Catalog = Agencia3;Integrated Security=true;";
         //Alex
-        public static string DB1 = "Data Source=DESKTOP-60PL41P\\SQLEXPRESS;Initial Catalog = Agencia1;Integrated Security=true;";
-        public static string BD2 = "Data Source=DESKTOP-60PL41P\\SQLEXPRESS;Initial Catalog = Agencia2;Integrated Security=true;";
-        public static string BD3 = "Data Source=DESKTOP-60PL41P\\SQLEXPRESS;Initial Catalog = Agencia3;Integrated Security=true;";
+        //public static string DB1 = "Data Source=DESKTOP-60PL41P\\SQLEXPRESS;Initial Catalog = Agencia1;Integrated Security=true;";
+        //public static string BD2 = "Data Source=DESKTOP-60PL41P\\SQLEXPRESS;Initial Catalog = Agencia2;Integrated Security=true;";
+        //public static string BD3 = "Data Source=DESKTOP-60PL41P\\SQLEXPRESS;Initial Catalog = Agencia3;Integrated Security=true;";
     }
 }
