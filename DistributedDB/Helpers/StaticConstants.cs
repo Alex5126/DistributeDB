b3d81ebda46ae23daf2e47220de7e3737aaba561
Jonathan Bravo <jonhdeveloper@outlook.com>
2018-06-19 00:56:30 -0500

    Connection string

---
+++ DistributedDB/Helpers/StaticConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedDB.Helpers
+{
+    public static class StaticConstants
+    {
+        public static string DB1 = "";
+        public static string BD2 = "";
+        public static string BD3 = "";
+    }
+}
